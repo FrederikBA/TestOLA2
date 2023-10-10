90a357376446fa7c3ec1d99c5e7628c1768690c7
FrederikBA <frederikbandersen98@gmail.com>
2023-10-10 13:30:41 +0200

    unittest - allones, one spare, one strike

--- BowlingTest/GameTests.cs
+++ BowlingTest/GameTests.cs
@@ -18,10 +18,42 @@ public class GameTests
         }
     }
     
+    //Helper method
+    private void RollSpare()
+    {
+        _bowlingGame.Roll(6,4);
+    }
+    
     [Fact]
     public void TestGutterGame()
     {
         RollMany(10, 0, 0);
         Assert.Equal(0, _bowlingGame.Score());
+    }
+    
+    [Fact]
+    public void TestAllOnes()
+    {
+        RollMany(10, 1, 0);
+        Assert.Equal(10, _bowlingGame.Score());
+    }
+    
+    [Fact]
+    public void TestOneSpare()
+    {
+        RollSpare();
+        _bowlingGame.Roll(3, 0);
+        RollMany(8, 0, 0);
+        Assert.Equal(16, _bowlingGame.Score());
+    }
+    
+    [Fact]
+    public void TestOneStrike()
+    {
+        _bowlingGame.Roll(10, 0);
+        _bowlingGame.Roll(4, 0);
+        _bowlingGame.Roll(5, 0);
+        RollMany(7, 1, 0);
+        Assert.Equal(30, _bowlingGame.Score());
     }
 }
