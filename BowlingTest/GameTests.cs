784473ae446cb1f81e5e3258429eda7ebd5d1434
FrederikBA <frederikbandersen98@gmail.com>
2023-10-10 13:20:40 +0200

    unittest - TestGutterGame

--- BowlingTest/GameTests.cs
+++ BowlingTest/GameTests.cs
@@ -2,8 +2,26 @@ namespace BowlingTest;
 
 public class GameTests
 {
-    [Fact]
-    public void Test1()
+    private readonly Game _bowlingGame;
+
+    public GameTests()
     {
+        _bowlingGame = new Game();
+    }
+
+    //Helper method
+    private void RollMany(int rolls, int pins1, int pin2, int pins3 = 0)
+    {
+        for (int i = 0; i < rolls; i++)
+        {
+            _bowlingGame.Roll(pins1, pin2, pins3);
+        }
+    }
+    
+    [Fact]
+    public void TestGutterGame()
+    {
+        RollMany(10, 0, 0);
+        Assert.Equal(0, _bowlingGame.Score());
     }
 }
